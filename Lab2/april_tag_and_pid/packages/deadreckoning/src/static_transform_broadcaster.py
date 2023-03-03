98404a36e66884aa40cd557ca33f80cf78065ec8
Khurramjaved <kjaved@ualberta.ca>
2023-03-03 14:19:30 -0700

    Correct the orientation of static frames

--- Lab2/april_tag_and_pid/packages/deadreckoning/src/static_transform_broadcaster.py
+++ Lab2/april_tag_and_pid/packages/deadreckoning/src/static_transform_broadcaster.py
@@ -10,11 +10,16 @@ from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 # http://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20static%20broadcaster%20%28Python%29
 
 
+PI = 3.1415
 apriltag_list = [200, 201, 94, 93, 153, 133, 58, 62, 169, 162]
 apriltag_name_list = ['200', '201', '94', '93',
                       '153', '133', '58', '62', '169', '162']
 x_list = [0.17, 1.65, 1.65, 0.17, 1.75, 1.253, 0.574, 0.075, 0.574, 1.253]
 y_list = [0.17, 0.17, 2.84, 2.84, 1.252, 1.755, 1.259, 1.755, 1.755, 1.253]
+yaw = [PI/2 + PI/4, PI/2 + 3*PI/4, -PI/4, PI/2 - PI/4, 0, PI, 0, PI, PI/2, -PI/2 ]
+pitch = [1.57]
+
+
 
 
 
@@ -35,7 +40,7 @@ if __name__ == '__main__':
         t.transform.translation.z = float(0)
 
         quat = tf.transformations.quaternion_from_euler( #TODO: search more about it
-            float(0), float(0), float(0))
+            float(-PI/2), float(0), yaw[i])
         t.transform.rotation.x = quat[0]
         t.transform.rotation.y = quat[1]
         t.transform.rotation.z = quat[2]
@@ -43,8 +48,8 @@ if __name__ == '__main__':
 
         broadcaster.sendTransform(t)
         # self.log(f"sent info for apriltag {apriltag_name_list[i]}")
-        rospy.sleep(1)
+        rospy.sleep(0.2)
         i+=1
-    rospy.spin()
+    # rospy.spin()
 
 
